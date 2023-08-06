4ca76fda904740ff1c2f4211a73ed8be5753d420
tahmidu <tahmid.uddin.dev@gmail.com>
2023-08-06 12:10:26 +0100

    Renderless component removed. Lazy loaded chonky libs differently

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,7 +1,6 @@
-import { Fragment, Suspense, lazy, useState } from "react";
+import { Suspense, lazy, useState } from "react";
 
 const LazyFileBrowser = lazy(() => import('../Components/FileBrowser'));
-const LazyDefaults = lazy(() => import("../Components/ChonkyDefaults"));
 
 export default function Home() {
     const [showBrowser, setBrowserVisibility] = useState(false);
@@ -9,9 +8,6 @@ export default function Home() {
     return (
         <article>
             <button onClick={() => setBrowserVisibility((prev) => !prev)}>Click Me</button>
-            <Suspense fallback={<Fragment />}>
-                <LazyDefaults />
-            </Suspense>
             <Suspense fallback={<span>loading...</span>}>
                 {showBrowser && <LazyFileBrowser />}
             </Suspense>
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,6 +1,15 @@
 import { createInertiaApp } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client'
 
+const onLoad = async () => {
+  const setChonkyDefaults = (await import('chonky')).setChonkyDefaults;
+  const ChonkyIconFA = (await import('chonky-icon-fontawesome')).ChonkyIconFA;
+
+  setChonkyDefaults({ iconComponent: ChonkyIconFA });
+}
+
+onLoad();
+
 createInertiaApp({
   resolve: (name) => {
     const pages = import.meta.glob('./Pages/**/*.jsx');
